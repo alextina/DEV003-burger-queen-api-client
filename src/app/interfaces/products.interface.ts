5c5cd33d2077fdf8b62cb46a4b8beadb28c0efac
Mari Cornelio <katherinacornelio@gmail.com>
2023-04-04 14:06:18 -0500

    Rendering products to the menu component - Creating service/products - Implementing OnInit - Using ngFor in the HTML

---
+++ src/app/interfaces/products.interface.ts
@@ -0,0 +1,17 @@
+export interface Products {
+  _id: string;
+  name: string;
+  price: number;
+  image: URL;
+  type: string;
+  dateEntry: Date;
+}
+
+// {
+//     "_id": 1,
+//     "name": "American Coffe",
+//     "price": "$ 5",
+//     "image": "#",
+//     "type": "breakfast",
+//     "dateEntry": "04/04/23"
+//   },
---
+++ src/app/service/products.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Products } from '../interfaces/products.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private http: HttpClient) {}
+  apiurl = 'http://localhost:5000/products';
+
+  getProducts(): Observable<Products[]> {
+    return this.http.get<Products[]>(this.apiurl);
+  }
+}
