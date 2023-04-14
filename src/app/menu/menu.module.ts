f87f8f0ba542a384767244fc6ce7360e34d8ba0f
alextina <castilloavilaa@gmail.com>
2023-04-14 18:10:33 -0500

    Updating order summary (cart) - Adding customer name and table number - Adding quantity of products in the order button - Adding methods in the shared-order component - Use of @Input and ngModel

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu.component';
 import { MenuRoutingModule } from './menu-routing.module';
 import { OrderComponent } from './order/order.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   // asociando componentes al módulo
   declarations: [MenuComponent, OrderComponent],
   // Asociando Modulos externos 
-  imports: [CommonModule,MenuRoutingModule],
+  imports: [CommonModule, MenuRoutingModule, FormsModule],
 })
-export class MenuModule {}
+export class MenuModule { }
--- src/app/menu/order/order.component.ts
+++ src/app/menu/order/order.component.ts
@@ -1,40 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ProductsQty } from 'src/app/interfaces/products.interface';
 import { SharedOrderService } from 'src/app/service/shared-order.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css'],
 })
-export class OrderComponent {
-  constructor(private orderSvc: SharedOrderService) {}
+export class OrderComponent implements OnInit {
+  constructor(
+    private orderSvc: SharedOrderService,
+    private toastr: ToastrService,
+  ) { }
+
+  @Input() clientName!: string;
+  @Input() clientTable!: number;
+
   productsOrder$ = this.orderSvc.productsOrder$;
   total$ = this.orderSvc.total$;
+  order$ = this.orderSvc.order$;
+  noProduct!: ProductsQty[];
 
+
+  ngOnInit() {
+    this.productsOrder$.subscribe((res) => this.noProduct = res)
+  }
+
+  // para eliminar productos
   delete(id: string) {
     this.orderSvc.deleteProduct(id);
   }
 
+  // para actualizar cantidad de productos
   update(operations: string, id: string) {
     this.orderSvc.qtyOperations(operations, id);
-    // const product: ProductsQty | undefined = this.orderSvc.findProductById(id);
-    // if (product) {
-    //   if (operations === 'minus' && product.qty > 0) {
-    //     product.qty = product.qty - 1;
-    //   }
-    //   if (operations === 'add') {
-    //     product.qty = product.qty + 1;
-    //   }
-    //   if (product.qty === 0) {
-    //     // this.orderSvc.deleteProduct(id);
-    //     this.delete(id);
-    //   }
-    // }
   }
 
   // para cerrar modal
   closeModal() {
     this.orderSvc.$modal.emit(false);
+  }
+
+  // enviar orden a carrito de compras
+  sendOrder() {
+    this.orderSvc.onClickAddOrder(this.clientName, this.clientTable);
+    this.order$.subscribe((res) => console.log(res))
+    this.toastr.info('Order sent.')
   }
 }
